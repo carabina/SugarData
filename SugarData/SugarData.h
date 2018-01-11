168f64b0d82e1dacdc0b91237b9777655ef70581
MAXIM KOLESNIK <makleso6@gmail.com>
2018-01-11 03:33:01 +0300

    initial

---
+++ SugarData/SugarData.h
@@ -0,0 +1,19 @@
+//
+//  SugarData.h
+//  SugarData
+//
+//  Created by MAXIM KOLESNIK on 11.01.2018.
+//  Copyright © 2018 Sugar and Candy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SugarData.
+FOUNDATION_EXPORT double SugarDataVersionNumber;
+
+//! Project version string for SugarData.
+FOUNDATION_EXPORT const unsigned char SugarDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SugarData/PublicHeader.h>
+
+
---
+++ SugarData/SugarData.swift
@@ -0,0 +1,13 @@
+//
+//  SugarData.swift
+//  SugarData
+//
+//  Created by MAXIM KOLESNIK on 11.01.2018.
+//  Copyright © 2018 Sugar and Candy. All rights reserved.
+//
+
+import Foundation
+
+public class SugarData {
+    
+}
